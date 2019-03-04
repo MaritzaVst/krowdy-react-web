9ec0e7b3122d1d7cf732567b4f141b8619fb5790
maritzavst <maritzasintitafur@gmail.com>
2019-03-03 21:24:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.png';
 import './App.css';
-
+import 'antd/dist/antd.css' 
+import './assets/sass/main.sass'
+import images from './config/image'
+// const { logo } = images
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      messageText : "I'm a text"
+	constructor(props) {
+		super(props)
+		this.state = {
+			messageText : "I'm a text"
 		}
-  }
-  render() {
-    return (
-      <div className="App">
-        <video autoPlay></video>
-      </div>
+  	}
+	render() {
+		console.log(images)
+		return (
+			<div className="App">
+				<header>
+					<img src={ logo }/>
+				</header>
+			</div>
 		);
-		
 	}
 	initMediaDevices() {
-		var p = navigator.mediaDevices.getUserMedia({ 
-			audio: { echoCancellation: false,  autoGainControl: true, sampleRate:48000, channelCount: 2, volume: 1.0 },
-			mandatory:{googAutoGainControl: false},
-			video: true 
-		});
-
-		p.then(function(mediaStream) {
-			var video = document.querySelector('video');
-			try {
-				video.srcObject = mediaStream
-				
-				console.log(mediaStream.getAudioTracks())
-
-			} catch(error) {
-				video.src = window.URL.createObjectURL(mediaStream);
-			}
-			video.onloadedmetadata = function(e) {
-				// Do something with the video here.
-			};
-		});
-		p.catch(function(err) { console.log(err.name); });
+		
 	}
 	componentDidMount() {
 		this.initMediaDevices()
---
+++ src/config/image.js
@@ -0,0 +1,6 @@
+const images = {
+    logo: './assets/images/logo.png'
+}
+
+
+export default images
