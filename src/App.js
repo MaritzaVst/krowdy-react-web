9e94d7b69fd86d43a7b5029795641325855b10cd
Maritza Victoria Sinti Tafur <maritza.sinti@tismart.com>
2019-03-04 14:21:36 -0500

    Base structure done

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import './App.css';
 import 'antd/dist/antd.css' 
 import './assets/sass/main.sass'
 import images from './config/image'
-import MaterialIcon, {colorPalette} from 'material-icons-react';
-import carousel from './components/Carousel'
+import MaterialIcon from 'material-icons-react';
+import CarouselComp from './components/Carousel'
+import { Button } from 'antd'
+
+
 // const { logo } = images
 class App extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			messageText : "I'm a text"
+			messageText : "I'm a text",
+			questionNumber: 1,
+			questionText: '¿Cuáles son las primeras diferencias que encuentras <br/> entre UX y UI?',
+			step: 2
 		}
   	}
 	render() {
@@ -20,10 +26,10 @@ class App extends Component {
 		return (
 			<div className="App">
 				<header>
-					<img src={ logo }/>
+					<img alt="" src={ logo }/>
 					<div className="user-info">
 						<p>Maritza Sinti</p>
-						<img src={ userImg }/>
+						<img alt="" src={ userImg }/>
 					</div>
 				</header>
 				<div className="second-header">
@@ -38,9 +44,21 @@ class App extends Component {
 					</div>
 					
 				</div>
-				<div className="blue-block"></div>
+				<div className="blue-block">
+					<h3>Cuestionario para Diseñador UX / UI</h3>
+					<div>
+						<span>Ver preguntas</span>
+						<Button className="view-module-btn">
+							<MaterialIcon icon="view_module" />
+						</Button>
+					</div>
+				</div>
+				<div className="title-block">
+					<h3 className="title-block__number">{ this.state.questionNumber }</h3>
+					<p className="title-block__text" dangerouslySetInnerHTML={{ __html: this.state.questionText }}></p>
+				</div>
 				<div>
-					<carousel/>
+					<CarouselComp></CarouselComp>
 				</div>
 			</div>
 		);
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Carousel } from 'antd';
 
-ReactDOM.render(
-    <Carousel effect="fade">
-      <div><h3>1</h3></div>
-      <div><h3>2</h3></div>
-      <div><h3>3</h3></div>
-      <div><h3>4</h3></div>
-    </Carousel>,
-    mountNode
-  );
+class CarouselComp extends React.Component{
+	render() {
+        return (
+			<Carousel effect="fade">
+				<div><h3>1</h3></div>
+				<div><h3>2</h3></div>
+				<div><h3>3</h3></div>
+				<div><h3>4</h3></div>
+			</Carousel>
+		)
+	}
+}
+
+export default CarouselComp
+    
